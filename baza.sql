0cd93d5725ad1821aea93d6b4119dfecc771e841
David <dr4266@student.uni-lj.si>
2016-11-08 20:32:55 +0100

    Fixed the SBServlet - should be working now, Uporabnik.id auto increments with a sequence

--- baza.sql
+++ baza.sql
@@ -42,7 +42,8 @@ CREATE TABLE "Kavarna" (
     id integer NOT NULL,
     name character varying(40) NOT NULL,
     latitude double precision NOT NULL,
-    longitude double precision NOT NULL
+    longitude double precision NOT NULL,
+    id_narocila integer
 );
 
 
@@ -68,21 +69,38 @@ ALTER TABLE "Napitek" OWNER TO postgres;
 
 CREATE TABLE "Narocilo" (
     id integer NOT NULL,
-    item_list character varying(50)[] NOT NULL,
+    item_list character varying,
     prep_time character varying(50),
     prep_status character varying(50),
-    payment_status character varying(50)
+    payment_status character varying(50),
+    id_napitek integer,
+    id_uporabnik integer,
+    id_kavarna integer
 );
 
 
 ALTER TABLE "Narocilo" OWNER TO postgres;
 
 --
+-- Name: uporabnik_autoinc; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE uporabnik_autoinc
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    MAXVALUE 1000000
+    CACHE 1;
+
+
+ALTER TABLE uporabnik_autoinc OWNER TO postgres;
+
+--
 -- Name: Uporabnik; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE "Uporabnik" (
-    id integer NOT NULL,
+    id integer DEFAULT nextval('uporabnik_autoinc'::regclass) NOT NULL,
     name character varying(30) NOT NULL,
     surname character varying(30) NOT NULL,
     email character varying(50),
@@ -98,7 +116,7 @@ ALTER TABLE "Uporabnik" OWNER TO postgres;
 -- Data for Name: Kavarna; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY "Kavarna" (id, name, latitude, longitude) FROM stdin;
+COPY "Kavarna" (id, name, latitude, longitude, id_narocila) FROM stdin;
 \.
 
 
@@ -114,7 +132,7 @@ COPY "Napitek" (id, prep_time, size, type) FROM stdin;
 -- Data for Name: Narocilo; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY "Narocilo" (id, item_list, prep_time, prep_status, payment_status) FROM stdin;
+COPY "Narocilo" (id, item_list, prep_time, prep_status, payment_status, id_napitek, id_uporabnik, id_kavarna) FROM stdin;
 \.
 
 
@@ -123,9 +141,16 @@ COPY "Narocilo" (id, item_list, prep_time, prep_status, payment_status) FROM std
 --
 
 COPY "Uporabnik" (id, name, surname, email, latitude, longitude, username) FROM stdin;
-1	David	Rubin	mail@david.si	48.235622999999997	47.346899999999998	rdavid
-2	Tilen	Venko	mail@tilen.si	47.899299999999997	23.989000000000001	zeljestudent
+1	David	Rubin	mail@rubin.si	48.433999999999997	32.231999999999999	pussslayer
+2	Tilen	Venko	mail@venko.si	46.872999999999998	40.542999999999999	zeljestudent
 \.
+
+
+--
+-- Name: uporabnik_autoinc; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('uporabnik_autoinc', 4, true);
 
 
 --
@@ -158,6 +183,38 @@ ALTER TABLE ONLY "Narocilo"
 
 ALTER TABLE ONLY "Uporabnik"
     ADD CONSTRAINT "Uporabnik_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: Narocilo id_kavarna; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY "Narocilo"
+    ADD CONSTRAINT id_kavarna FOREIGN KEY (id_kavarna) REFERENCES "Kavarna"(id);
+
+
+--
+-- Name: Narocilo id_napitek; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY "Narocilo"
+    ADD CONSTRAINT id_napitek FOREIGN KEY (id_napitek) REFERENCES "Napitek"(id);
+
+
+--
+-- Name: Narocilo id_uporabnik; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY "Narocilo"
+    ADD CONSTRAINT id_uporabnik FOREIGN KEY (id_uporabnik) REFERENCES "Uporabnik"(id);
+
+
+--
+-- Name: Kavarna narociloF_key; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY "Kavarna"
+    ADD CONSTRAINT "narociloF_key" FOREIGN KEY (id_narocila) REFERENCES "Narocilo"(id);
 
 
 --
--- prpo-vaje/Klik-kafe-web/src/main/java/TestSBServlet.java
+++ prpo-vaje/Klik-kafe-web/src/main/java/TestSBServlet.java
@@ -1,6 +1,7 @@
 
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -8,7 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import si.fri.prpo.zrna.UpravljalecUporabnikovSB;
+import si.fri.prpo.vaje.narocanje.entitete.Uporabnik;
+import si.fri.prpo.zrna.UpravljalecUporabnikovSBLocal;
 
 /**
  * Servlet implementation class TestSBServlet
@@ -17,8 +19,9 @@ import si.fri.prpo.zrna.UpravljalecUporabnikovSB;
 
 public class TestSBServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
 	@EJB
-	private UpravljalecUporabnikovSB upravlalecUporabnikov;
+	private UpravljalecUporabnikovSBLocal upravlalecUporabnikov;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -32,9 +35,17 @@ public class TestSBServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append("\n\nUporabniki v bazi:\n");
 		upravlalecUporabnikov.returnAll(response);
-		upravlalecUporabnikov.addUser("hudogriz", "Blaz", "Vrhovsek", "blaz.vrhovske@gmail.com", 3);
+		response.getWriter().append("\n");
+		
+		response.getWriter().append("\nDodajam uporabnika Blaz\n");
+		upravlalecUporabnikov.addUser("hudogriz", "Blaz", "Vrhovsek", "mail@vrhi.si", 24.3532, 25.6432 );
+		upravlalecUporabnikov.returnAll(response);
+		response.getWriter().append("\n");
+
+		response.getWriter().append("Brisem uporabnika z imenom \"Blaz\"\n");
+		upravlalecUporabnikov.deleteUser("Blaz");
 		upravlalecUporabnikov.returnAll(response);
 	}
 
