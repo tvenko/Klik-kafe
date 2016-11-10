800fbb163545dcfbde9a8670afe82ffbce745d18
David <dr4266@student.uni-lj.si>
2016-11-10 14:38:19 +0100

    Updated the database - new connections and some new atributes

--- baza.sql
+++ baza.sql
@@ -42,8 +42,7 @@ CREATE TABLE "Kavarna" (
     id integer NOT NULL,
     name character varying(40) NOT NULL,
     latitude double precision NOT NULL,
-    longitude double precision NOT NULL,
-    id_narocila integer
+    longitude double precision NOT NULL
 );
 
 
@@ -79,6 +78,60 @@ CREATE TABLE "Napitek" (
 ALTER TABLE "Napitek" OWNER TO postgres;
 
 --
+-- Name: napitki_kavarne_autoinc; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE napitki_kavarne_autoinc
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    MAXVALUE 10000000
+    CACHE 1;
+
+
+ALTER TABLE napitki_kavarne_autoinc OWNER TO postgres;
+
+--
+-- Name: Napitki_kavarne; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE "Napitki_kavarne" (
+    id integer DEFAULT nextval('napitki_kavarne_autoinc'::regclass) NOT NULL,
+    id_napitka integer NOT NULL,
+    id_kavarna integer NOT NULL
+);
+
+
+ALTER TABLE "Napitki_kavarne" OWNER TO postgres;
+
+--
+-- Name: napitki_narocila_autoinc; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE napitki_narocila_autoinc
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    MAXVALUE 10000000
+    CACHE 1;
+
+
+ALTER TABLE napitki_narocila_autoinc OWNER TO postgres;
+
+--
+-- Name: Napitki_narocila; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE "Napitki_narocila" (
+    id integer DEFAULT nextval('napitki_narocila_autoinc'::regclass) NOT NULL,
+    id_narocila integer NOT NULL,
+    id_napitka integer NOT NULL
+);
+
+
+ALTER TABLE "Napitki_narocila" OWNER TO postgres;
+
+--
 -- Name: narocilo_autoinc; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -98,13 +151,12 @@ ALTER TABLE narocilo_autoinc OWNER TO postgres;
 
 CREATE TABLE "Narocilo" (
     id integer DEFAULT nextval('narocilo_autoinc'::regclass) NOT NULL,
-    item_list character varying,
     prep_time character varying(50),
     prep_status character varying(50),
     payment_status character varying(50),
-    id_napitek integer,
-    id_uporabnik integer,
-    id_kavarna integer
+    id_uporabnik integer NOT NULL,
+    id_kavarna integer NOT NULL,
+    total_price double precision
 );
 
 
@@ -145,8 +197,10 @@ ALTER TABLE "Uporabnik" OWNER TO postgres;
 -- Data for Name: Kavarna; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY "Kavarna" (id, name, latitude, longitude, id_narocila) FROM stdin;
-1	marjetica	1234.8900000000001	1234.8900000000001	\N
+COPY "Kavarna" (id, name, latitude, longitude) FROM stdin;
+1	Marjetica	1234.8900000000001	1234.8900000000001
+2	Beli Konj	56.43	32.670000000000002
+3	Gostilna Pri Inotu	55.219999999999999	40.759999999999998
 \.
 
 
@@ -168,13 +222,49 @@ COPY "Napitek" (id, prep_time, size, type, price) FROM stdin;
 
 
 --
+-- Data for Name: Napitki_kavarne; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "Napitki_kavarne" (id, id_napitka, id_kavarna) FROM stdin;
+1	2	1
+2	5	1
+3	4	1
+4	3	1
+5	6	1
+6	7	1
+7	7	2
+8	1	2
+9	2	2
+10	3	2
+11	9	2
+12	9	3
+13	8	3
+14	7	3
+\.
+
+
+--
+-- Data for Name: Napitki_narocila; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "Napitki_narocila" (id, id_narocila, id_napitka) FROM stdin;
+1	1	1
+2	1	4
+3	1	8
+4	2	5
+5	3	2
+6	3	9
+\.
+
+
+--
 -- Data for Name: Narocilo; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY "Narocilo" (id, item_list, prep_time, prep_status, payment_status, id_napitek, id_uporabnik, id_kavarna) FROM stdin;
-2	kapucino	120s	in progress	waiting	1	1	1
-3	kapucino	120s	in progress	waiting	1	1	1
-4	kapucino	120s	in progress	waiting	1	1	1
+COPY "Narocilo" (id, prep_time, prep_status, payment_status, id_uporabnik, id_kavarna, total_price) FROM stdin;
+2	90	done	done	2	3	1.5
+1	120	pending	done	1	1	3.5
+3	180	done	pending	1	2	2.8999999999999999
 \.
 
 
@@ -196,17 +286,31 @@ SELECT pg_catalog.setval('napitek_autoinc', 9, true);
 
 
 --
+-- Name: napitki_kavarne_autoinc; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('napitki_kavarne_autoinc', 14, true);
+
+
+--
+-- Name: napitki_narocila_autoinc; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('napitki_narocila_autoinc', 1, false);
+
+
+--
 -- Name: narocilo_autoinc; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('narocilo_autoinc', 4, true);
+SELECT pg_catalog.setval('narocilo_autoinc', 275, true);
 
 
 --
 -- Name: uporabnik_autoinc; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('uporabnik_autoinc', 30, true);
+SELECT pg_catalog.setval('uporabnik_autoinc', 307, true);
 
 
 --
@@ -226,6 +330,22 @@ ALTER TABLE ONLY "Napitek"
 
 
 --
+-- Name: Napitki_kavarne Napitki_kavarne_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY "Napitki_kavarne"
+    ADD CONSTRAINT "Napitki_kavarne_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: Napitki_narocila Napitki_narocila_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY "Napitki_narocila"
+    ADD CONSTRAINT "Napitki_narocila_pkey" PRIMARY KEY (id);
+
+
+--
 -- Name: Narocilo Narocilo_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -242,6 +362,14 @@ ALTER TABLE ONLY "Uporabnik"
 
 
 --
+-- Name: Napitki_kavarne id_kavarna; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY "Napitki_kavarne"
+    ADD CONSTRAINT id_kavarna FOREIGN KEY (id_kavarna) REFERENCES "Kavarna"(id);
+
+
+--
 -- Name: Narocilo id_kavarna; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -250,11 +378,27 @@ ALTER TABLE ONLY "Narocilo"
 
 
 --
--- Name: Narocilo id_napitek; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: Napitki_narocila id_napitka; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY "Narocilo"
-    ADD CONSTRAINT id_napitek FOREIGN KEY (id_napitek) REFERENCES "Napitek"(id);
+ALTER TABLE ONLY "Napitki_narocila"
+    ADD CONSTRAINT id_napitka FOREIGN KEY (id_napitka) REFERENCES "Napitek"(id);
+
+
+--
+-- Name: Napitki_kavarne id_napitka; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY "Napitki_kavarne"
+    ADD CONSTRAINT id_napitka FOREIGN KEY (id_napitka) REFERENCES "Napitek"(id);
+
+
+--
+-- Name: Napitki_narocila id_narocila; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY "Napitki_narocila"
+    ADD CONSTRAINT id_narocila FOREIGN KEY (id_narocila) REFERENCES "Narocilo"(id);
 
 
 --
@@ -263,14 +407,6 @@ ALTER TABLE ONLY "Narocilo"
 
 ALTER TABLE ONLY "Narocilo"
     ADD CONSTRAINT id_uporabnik FOREIGN KEY (id_uporabnik) REFERENCES "Uporabnik"(id);
-
-
---
--- Name: Kavarna narociloF_key; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY "Kavarna"
-    ADD CONSTRAINT "narociloF_key" FOREIGN KEY (id_narocila) REFERENCES "Narocilo"(id);
 
 
 --
